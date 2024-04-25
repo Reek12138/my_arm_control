5c1cd427078e81b660a8782cc075b7dd37fed4c5
Reek12138 <2894487027@qq.com>
2024-04-25 18:07:49 +0800

    Add interbotix_ros_arms as a submodule

---
+++ build/interbotix_ros_arms/interbotix_examples/interbotix_diagnostic_tool/CMakeFiles/interbotix_diagnostic_tool_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/reek/interbotix_ws/devel/lib/python2.7/dist-packages/interbotix_diagnostic_tool/msg/_MotorTemps.py"
+  "/home/reek/interbotix_ws/devel/lib/python2.7/dist-packages/interbotix_diagnostic_tool/msg/__init__.py"
+  "CMakeFiles/interbotix_diagnostic_tool_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/interbotix_diagnostic_tool_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/interbotix_ros_arms/interbotix_sdk/catkin_generated/python_distutils_install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ -n "$DESTDIR" ] ; then
+    case $DESTDIR in
+        /*) # ok
+            ;;
+        *)
+            /bin/echo "DESTDIR argument must be absolute... "
+            /bin/echo "otherwise python's distutils will bork things."
+            exit 1
+    esac
+fi
+
+echo_and_run() { echo "+ $@" ; "$@" ; }
+
+echo_and_run cd "/home/reek/interbotix_ws/src/interbotix_ros_arms/interbotix_sdk"
+
+# ensure that Python install destination exists
+echo_and_run mkdir -p "$DESTDIR/home/reek/interbotix_ws/install/lib/python2.7/dist-packages"
+
+# Note that PYTHONPATH is pulled from the environment to support installing
+# into one location when some dependencies were installed in another
+# location, #123.
+echo_and_run /usr/bin/env \
+    PYTHONPATH="/home/reek/interbotix_ws/install/lib/python2.7/dist-packages:/home/reek/interbotix_ws/build/lib/python2.7/dist-packages:$PYTHONPATH" \
+    CATKIN_BINARY_DIR="/home/reek/interbotix_ws/build" \
+    "/usr/bin/python2" \
+    "/home/reek/interbotix_ws/src/interbotix_ros_arms/interbotix_sdk/setup.py" \
+     \
+    build --build-base "/home/reek/interbotix_ws/build/interbotix_ros_arms/interbotix_sdk" \
+    install \
+    --root="${DESTDIR-/}" \
+    --install-layout=deb --prefix="/home/reek/interbotix_ws/install" --install-scripts="/home/reek/interbotix_ws/install/bin"
